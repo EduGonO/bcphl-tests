e3921ed03c4445b663df643226dbd516736a766e
edu gonzalez <46654892+EduGonO@users.noreply.github.com>
2025-05-02 16:50:49 +0200

    -header

--- pages/categories/[category].tsx
+++ pages/categories/[category].tsx
@@ -45,11 +45,6 @@ export default function CategoryEntry({ category, articles }: CategoryEntryProps
 
   return (
     <div style={{ padding: '2rem' }}>
-    
-  <Header
-          categories={category}
-          onCategoryChange={handleCategoryChange}
-        />
       <h1 style={{ color: config.color }}>{category}</h1>
       {config.media.map((src, i) => (
         <img key={i} src={src} alt={`Media ${i}`} style={{ maxWidth: '100%', marginBottom: '1rem' }} />
