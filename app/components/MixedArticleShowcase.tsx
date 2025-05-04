0289bdf31a940b9966d4ece9a8b6d0b321be4fa8
edu gonzalez <46654892+EduGonO@users.noreply.github.com>
2025-05-04 18:32:55 +0200

    ya

--- app/components/MixedArticleShowcase.tsx
+++ app/components/MixedArticleShowcase.tsx
@@ -76,9 +76,9 @@ const MixedArticleShowcase: React.FC<MixedArticleShowcaseProps> = ({
 
         @media(min-width:768px) {
           .mas { flex-direction: row; }
-          .left   { order: 0; flex: 1; max-width: 300px; }
-          .center { order: 1; flex: 2; }
-          .left   { order: 0; flex: 1; max-width: 300px; }
+          .left   { order: 0; flex: 1 1 20%; }
+          .center { order: 1; flex: 1 1 60%; }
+          .right  { order: 2; flex: 1 1 20%; }
         }
 
         .col {
