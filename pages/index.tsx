f8ea72aba2fc49f32c4bc7133de53e96a4df1ae1
edu gonzalez <46654892+EduGonO@users.noreply.github.com>
2025-05-04 17:29:06 +0200

    .erf

--- pages/index.tsx
+++ pages/index.tsx
@@ -89,7 +89,7 @@ const Home: React.FC<HomeProps> = ({ articles, categories }) => {
               titleFont="GayaRegular"
             />
             <CategoryArticleCompact
-  articles={allArticles}
+  articles={articles}
   categories={allCategories}
   titleFont="GayaRegular"
 />
